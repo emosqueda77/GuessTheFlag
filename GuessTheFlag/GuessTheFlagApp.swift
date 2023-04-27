25f67d929c745f279da17ebe0bf5d2a99efd53ea
Enrique Mosqueda <emosqueda77@yahoo.com>
2023-04-27 11:57:43 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Enrique Mosqueda on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
